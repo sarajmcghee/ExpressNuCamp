8d3151d9e7b0aa056a4e6f09a90018231ef2a434
Sara McGhee <sarajmcghee@gmail.com>
2022-04-08 12:11:16 -0400

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -37,6 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
+
 app.use(session({
     name: 'session-id',
     secret: '12345-67890-09876-54321',
@@ -44,25 +47,22 @@ app.use(session({
     resave: false,
     store: new FileStore()
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-    if (!req.session.user) {
-        const err = new Error('You are not authenticated!');
+    if (!req.user) {
+        const err = new Error('You are not authenticated!');                    
         err.status = 401;
         return next(err);
     } else {
-        if (req.session.user === 'authenticated') {
-            return next();
-        } else {
-            const err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-        }
+        return next();
     }
 }
 
